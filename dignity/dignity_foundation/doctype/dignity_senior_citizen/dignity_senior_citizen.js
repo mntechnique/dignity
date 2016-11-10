4453b30310299fd81605b46e9c2ebaca44770f49
pawan <pawan@erpnext.com>
2016-11-10 18:43:24 +0530

    dignity

--- dignity/dignity_foundation/doctype/dignity_senior_citizen/dignity_senior_citizen.js
+++ dignity/dignity_foundation/doctype/dignity_senior_citizen/dignity_senior_citizen.js
@@ -5,20 +5,21 @@ frappe.ui.form.on('Dignity Senior Citizen', {
 	refresh: function(frm) {
 	}
 });
-/*
+
 frappe.ui.form.on("Dignity Senior Citizen", {
 	tahsildar_centre: function(frm, cdt, cdn) {
 		var senior_citizen = frappe.model.get_doc(cdt, cdn);
 		if (senior_citizen.tahsildar_centre)
 		{			frappe.call({
-				method: "dignity.dignity_foundation.doctype.dignity_senior_citizen.dignity_senior_citizen.get_tahsildar",
+				method: "dignity.dignity_foundation.doctype.dignity_senior_citizen.dignity_senior_citizen.get_card_series",
 				args: {
 					state: frm.doc.tahsildar_state,
 					city: frm.doc.tahsildar_city,
 					centre: frm.doc.tahsildar_centre
 				},
 				callback: function(r) {
-					frappe.model.set_value(cdt, cdn, "naming_series", r.message[0]);
+					frappe.model.set_value(cdt, cdn, "new_series_no", r.message[0]);
+					frappe.model.set_value(cdt, cdn, "card_number", r.message[1]);
 				}
 			});
 		}
@@ -27,14 +28,15 @@ frappe.ui.form.on("Dignity Senior Citizen", {
 		var senior_citizen = frappe.model.get_doc(cdt, cdn);
 		if (senior_citizen.tahsildar_centre)
 		{			frappe.call({
-				method: "dignity.dignity_foundation.doctype.dignity_senior_citizen.dignity_senior_citizen.get_tahsildar",
+				method: "dignity.dignity_foundation.doctype.dignity_senior_citizen.dignity_senior_citizen.get_card_series",
 				args: {
 					state: frm.doc.tahsildar_state,
 					city: frm.doc.tahsildar_city,
 					centre: frm.doc.tahsildar_centre
 				},
 				callback: function(r) {
-					frappe.model.set_value(cdt, cdn, "naming_series", r.message[0]);
+					frappe.model.set_value(cdt, cdn, "new_series_no", r.message[0]);
+					frappe.model.set_value(cdt, cdn, "card_number", r.message[1]);
 				}
 			});
 		}
@@ -43,20 +45,54 @@ frappe.ui.form.on("Dignity Senior Citizen", {
 		var senior_citizen = frappe.model.get_doc(cdt, cdn);
 		if (senior_citizen.tahsildar_centre)
 		{			frappe.call({
-				method: "dignity.dignity_foundation.doctype.dignity_senior_citizen.dignity_senior_citizen.get_tahsildar",
+				method: "dignity.dignity_foundation.doctype.dignity_senior_citizen.dignity_senior_citizen.get_card_series",
 				args: {
 					state: frm.doc.tahsildar_state,
 					city: frm.doc.tahsildar_city,
 					centre: frm.doc.tahsildar_centre
 				},
 				callback: function(r) {
-					frappe.model.set_value(cdt, cdn, "naming_series", r.message[0]);
+					frappe.model.set_value(cdt, cdn, "new_series_no", r.message[0]);
+					frappe.model.set_value(cdt, cdn, "card_number", r.message[1]);
 				}
 			});
 		}
+ 	},
+	tahsildar_name: function(frm, cdt, cdn) {
+		var senior_citizen = frappe.model.get_doc(cdt, cdn);
+		if (senior_citizen.tahsildar_centre)
+		{			frappe.call({
+				method: "dignity.dignity_foundation.doctype.dignity_senior_citizen.dignity_senior_citizen.get_tahsildar_series",
+				args: {
+					state: frm.doc.tahsildar_state,
+					city: frm.doc.tahsildar_city,
+					tahsildar_name: frm.doc.tahsildar_name
+				},
+				callback: function(r) {
+					frappe.model.set_value(cdt, cdn, "new_tahsildar_series", r.message[0]);
+					frappe.model.set_value(cdt, cdn, "tahsildar_no", r.message[1]);
+				}
+			});
+		}
+ 	},
+	card_series_start: function(frm, cdt, cdn) {
+		var senior_citizen = frappe.model.get_doc(cdt, cdn);
+		if (senior_citizen.card_series_start)
+		{ 
+			var cardnumber = senior_citizen.card_series_start + senior_citizen.new_series_no;
+			frappe.model.set_value(cdt,cdn,"card_number",cardnumber);
+		}
+ 	},
+	new_series_no: function(frm, cdt, cdn) {
+		var senior_citizen = frappe.model.get_doc(cdt, cdn);
+		if (senior_citizen.new_series_no)
+		{ 
+			var cardnumber = senior_citizen.card_series_start + senior_citizen.new_series_no;
+			frappe.model.set_value(cdt,cdn,"card_number",cardnumber);
+		}
  	}
 });
-*/
+
 frappe.ui.form.on("Dignity Senior Citizen", {
 	date_of_birth: function(frm, cdt, cdn) {
 		var senior_citizen = frappe.model.get_doc(cdt, cdn);
--- dignity/dignity_foundation/doctype/dignity_senior_citizen/dignity_senior_citizen.py
+++ dignity/dignity_foundation/doctype/dignity_senior_citizen/dignity_senior_citizen.py
@@ -15,64 +15,98 @@ class DignitySeniorCitizen(Document):
 		if not self.tahsildar_state or not self.tahsildar_city or not self.tahsildar_centre:
 			frappe.throw(_("State, City and Card Center are mandatory fields"))
 		if not self.tahsildar_state or not self.tahsildar_city or not self.tahsildar_name:
-				frappe.throw(_("State, City and Tahsildar Name are mandatory fields"))
-		#Check Card Center Combination
-		if self.tahsildar_state and self.tahsildar_city and self.tahsildar_centre:
-			card = frappe.db.sql("""select naming_series,card_series_from, card_series_to,card_last_series from `tabDignity Card Series Master` where parent = %s and tahsildar_city = %s and card_centre = %s""",(self.tahsildar_state,self.tahsildar_city,self.tahsildar_centre))
-			if card:
-				naming_series = card[0][0]
-				card_series_from = card[0][1]
-				card_series_to = card[0][2]
-				card_last_series_no = card[0][3]
+			frappe.throw(_("State, City and Tahsildar Name are mandatory fields"))
+		def gen_update_card_series(state,city,centre,card_number):
+			#Check if card number already exists
+			if card_number:
+				#check if card number already exists to avoid data duplication
+				card_check = frappe.db.sql("""select name from `tabDignity Senior Citizen` where card_number = %s""",card_number)
+				if card_check:
+					name = card_check[0][0]
+					if self.name == name:
+						pass
+					else:
+						frappe.throw(_("Card Number already in use"))
+				else:
+					card = frappe.db.sql("""select card_last_series from `tabDignity Card Series Master` where parent = %s and tahsildar_city = %s and card_centre = %s""",(state,city,centre))
+					if card:
+						card_last_series_no = card[0][0]
+						if self.new_series_no < card_last_series_no:
+							pass
+						else:
+							frappe.db.sql("""update `tabDignity Card Series Master` set card_last_series = %s where parent = %s and tahsildar_city = %s and card_centre = %s""",(self.new_series_no,state,city,centre))
 			else:
-				frappe.throw(_("Number series is not set up for State, City and Centre combination."))
-		#Check Tahsildar Combination
-		if self.tahsildar_state and self.tahsildar_city and self.tahsildar_name:
-			tahsildar = frappe.db.sql("""select tahsildar_series_from,tahsildar_last_series from `tabDignity Tahsildar Master` where parent = %s and tahsildar_city = %s and tahsildar_centre = %s""",(self.tahsildar_state,self.tahsildar_city,self.tahsildar_name))
-			if tahsildar:
-				tahsildar_series_from = tahsildar[0][0]
-				tahsildar_last_series = tahsildar[0][1]
-			else:
-				frappe.throw(_("Number series is not set up for State, City and Tahsildar Centre combination."))
-		# Generate the card Number, validate range and Insert
-			new_series_no = card_last_series_no + 1
-			new_tahsildar_no = tahsildar_last_series + 1
-		if new_series_no > card_series_to:
-			frappe.throw(_("You have used all existing series numbers in the setup. Please update series number in Master Set Up accordingly"))
-		str_len_new_series_no = len(cstr(new_series_no))
-		str_new_series_no = cstr(new_series_no)
-		#new tahsildar series no
-		str_len_new_tahsildar_series_no = len(cstr(new_tahsildar_no))
-		str_new_tahsildar_series_no = cstr(new_tahsildar_no)
-		#zero padding center series
-		if str_len_new_series_no < 5:
-			no_of_leading_zeroes = 5 - str_len_new_series_no
-			str_new_series_no.ljust(no_of_leading_zeroes,"0")
-		#zero padding tahsildar series
-		if str_len_new_tahsildar_series_no < 5:
-			no_of_leading_zeroes = 5 - str_len_new_tahsildar_series_no
-			str_new_tahsildar_series_no.ljust(no_of_leading_zeroes,"0")
-		#new card number
-		new_card_no = naming_series + str_new_series_no
-		self.new_series_no = new_series_no
-		self.card_number = new_card_no
-		#new tahsildar number
-		self.tahsildar_no = str_new_tahsildar_series_no
-		#check if card number already exists to avoid data duplication
-		card_check = frappe.db.sql("""select 'X' from `tabDignity Senior Citizen` where card_number = %s""",self.card_number)
-		#check if tahsildar series number already exists to avoid data duplication
-		tahsildar_check = frappe.db.sql("""select 'X' from `tabDignity Senior Citizen` where tahsildar_no = %s""",self.tahsildar_no)
-		if card_check:
-			frappe.throw(_("Senior Citizen Card No already exists"))
-		if tahsildar_check:
-			frappe.throw(_("Tahsildar No already exists"))
-		if self.tahsildar_state and self.tahsildar_city and self.tahsildar_centre:
-			frappe.db.sql("""update `tabDignity Card Series Master` set card_last_series = %s where parent = %s and tahsildar_city = %s and card_centre = %s""",(self.new_series_no,self.tahsildar_state,self.tahsildar_city,self.tahsildar_centre))
-		if self.tahsildar_state and self.tahsildar_city and self.tahsildar_name:
-			frappe.db.sql("""update `tabDignity Tahsildar Master` set tahsildar_last_series = %s where parent = %s and tahsildar_city = %s and tahsildar_centre = %s""",(self.tahsildar_no,self.tahsildar_state,self.tahsildar_city,self.tahsildar_name))
+				#Check Card Center Combination
+				card = frappe.db.sql("""select naming_series,card_series_from, card_series_to,card_last_series from `tabDignity Card Series Master` where parent = %s and tahsildar_city = %s and card_centre = %s""",(state,city,centre))
+				if card:
+					naming_series = card[0][0]
+					card_series_from = card[0][1]
+					card_series_to = card[0][2]
+					card_last_series_no = card[0][3]
+					# Generate the card Number, validate range and Insert
+					new_series_no = card_last_series_no + 1
+					if new_series_no > card_series_to:
+						frappe.throw(_("You have used all existing series numbers in the setup. Please update series number in Master Set Up accordingly"))
+					else:
+						str_len_new_series_no = len(cstr(new_series_no))
+						str_new_series_no = cstr(new_series_no)
+						#zero padding center series
+						if str_len_new_series_no < 5:
+							no_of_leading_zeroes = 5 - str_len_new_series_no
+							str_new_series_no.ljust(no_of_leading_zeroes,"0")
+							#new card number
+							new_card_no = naming_series + str_new_series_no
+							self.new_series_no = new_series_no
+							self.card_number = new_card_no
+							frappe.db.sql("""update `tabDignity Card Series Master` set card_last_series = %s where parent = %s and tahsildar_city = %s and card_centre = %s""",(self.new_series_no,state,city,centre))
+				else:
+					frappe.throw(_("Number series is not set up for State, City and Centre combination."))
+				
+		def gen_update_tahsildar_series(state,city,tahsildar_name,tahsildar_number):
+			if tahsildar_number:
+				#check if tahsildar series number already exists to avoid data duplication
+				tahsildar_check = frappe.db.sql("""select name from `tabDignity Senior Citizen` where tahsildar_no = %s""",tahsildar_number)
+				if tahsildar_check:
+					name = tahsildar_check[0][0]
+					if self.name == name:
+						pass
+					else:
+						frappe.throw(_("Tahsildar Number already in use"))
+				else:
+					tahsildar = frappe.db.sql("""select tahsildar_last_series from `tabDignity Tahsildar Master` where parent = %s and tahsildar_city = %s and tahsildar_centre = %s""",(state,city,tahsildar_name))
+					if tahsildar:
+						tahsildar_last_series = tahsildar[0][0]
+						if self.tahsildar_no < tahsildar_last_series:
+							pass
+						else:
+							frappe.db.sql("""update `tabDignity Tahsildar Master` set tahsildar_last_series = %s where parent = %s and tahsildar_city = %s and tahsildar_centre = %s""",(self.tahsildar_no,state,city,tahsildar_name))
+					else:
+						#Check Tahsildar Combination
+						tahsildar = frappe.db.sql("""select tahsildar_series_from,tahsildar_last_series from `tabDignity Tahsildar Master` where parent = %s and tahsildar_city = %s and tahsildar_centre = %s""",(state,city,tahsildar_name))
+						if tahsildar:
+							tahsildar_series_from = tahsildar[0][0]
+							tahsildar_last_series = tahsildar[0][1]
+							new_tahsildar_no = tahsildar_last_series + 1
+							self.tahsildar_new_series = new_tahsildar_no
+							#new tahsildar series no
+							str_len_new_tahsildar_series_no = len(cstr(new_tahsildar_no))
+							str_new_tahsildar_series_no = cstr(new_tahsildar_no)
+							#zero padding tahsildar series
+							if str_len_new_tahsildar_series_no < 5:
+								no_of_leading_zeroes = 5 - str_len_new_tahsildar_series_no
+								str_new_tahsildar_series_no.ljust(no_of_leading_zeroes,"0")
+								#new tahsildar number
+							self.tahsildar_no = str_new_tahsildar_series_no
+							frappe.db.sql("""update `tabDignity Tahsildar Master` set tahsildar_last_series = %s where parent = %s and tahsildar_city = %s and tahsildar_centre = %s""",(self.tahsildar_no,state,city,tahsildar_name))
+						else:
+							frappe.throw(_("Number series is not set up for State, City and Tahsildar Centre combination."))
+		print("call update card series",self.tahsildar_state,self.tahsildar_city,self.tahsildar_centre,self.card_number)
+		gen_update_card_series(self.tahsildar_state,self.tahsildar_city,self.tahsildar_centre,self.card_number)
+		print("call update tahsildar series",self.tahsildar_state,self.tahsildar_city,self.tahsildar_name,self.tahsildar_no)
+		gen_update_tahsildar_series(self.tahsildar_state,self.tahsildar_city,self.tahsildar_name,self.tahsildar_no)
 
 @frappe.whitelist()
-def get_tahsildar(state,city,centre):
+def get_card_series(state,city,centre):
 	conditions=""
 	if state and city and centre:
 		filters = {}
@@ -85,5 +119,49 @@ def get_tahsildar(state,city,centre):
 			conditions += " and tahsildar_city = %(city)s"
 		if centre:
 			conditions += " and card_centre = %(centre)s"
-		card = frappe.db.sql("""select naming_series from `tabDignity Card Series Master` where 1=1 %s""" % (conditions,),filters)
-	return (card[0][0]) if card else ("MU")
+
+	#Check Card Center Combination
+		card = frappe.db.sql("""select naming_series,card_series_from, card_series_to,card_last_series from `tabDignity Card Series Master` where 1=1 %s""" % (conditions,),filters)
+		if card:
+			naming_series = card[0][0]
+			card_series_from = card[0][1]
+			card_series_to = card[0][2]
+			card_last_series_no = card[0][3]
+		# Generate the card Number, validate range and Insert
+			new_series_no = card_last_series_no + 1
+			if new_series_no > card_series_to:
+				frappe.throw(_("You have used all existing series numbers in the setup. Please update series number in Master Set Up accordingly"))
+			else:
+				str_len_new_series_no = len(cstr(new_series_no))
+				str_new_series_no = cstr(new_series_no)
+			#zero padding center series
+				if str_len_new_series_no < 5:
+					no_of_leading_zeroes = 5 - str_len_new_series_no
+					str_new_series_no.ljust(no_of_leading_zeroes,"0")
+			#new card number
+				new_card_no = naming_series + str_new_series_no
+				return (new_series_no,new_card_no)
+		else:
+			frappe.throw(_("Number series is not set up for State, City and Centre combination."))
+			
+@frappe.whitelist()
+def get_tahsildar_series(state,city,tahsildar_name):
+	#Check Tahsildar Combination
+		tahsildar = frappe.db.sql("""select tahsildar_series_from,tahsildar_last_series from `tabDignity Tahsildar Master` where parent = %s and tahsildar_city = %s and tahsildar_centre = %s""",(state,city,tahsildar_name))
+		if tahsildar:
+			tahsildar_series_from = tahsildar[0][0]
+			tahsildar_last_series = tahsildar[0][1]
+			#new tahsildar series no
+			new_tahsildar_no = tahsildar_last_series + 1
+			str_len_new_tahsildar_series_no = len(cstr(new_tahsildar_no))
+			str_new_tahsildar_series_no = cstr(new_tahsildar_no)
+			#zero padding tahsildar series
+			if str_len_new_tahsildar_series_no < 5:
+				no_of_leading_zeroes = 5 - str_len_new_tahsildar_series_no
+				str_new_tahsildar_series_no.ljust(no_of_leading_zeroes,"0")
+			#new tahsildar number
+			tahsildar_no = str_new_tahsildar_series_no
+			return (new_tahsildar_no,tahsildar_no)
+		else:
+			frappe.throw(_("Number series is not set up for State, City and Tahsildar combination."))
+	
