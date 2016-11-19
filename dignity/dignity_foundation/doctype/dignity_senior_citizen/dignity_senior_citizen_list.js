d4009a1cb9e7527cffae8547f05ec9089217d6f9
Gaurav Naik <gvrvnk@gmail.com>
2016-11-19 13:37:31 +0530

    Added year calculation change and hid Bulk Print Marathi button

--- dignity/dignity_foundation/doctype/dignity_senior_citizen/dignity_senior_citizen_list.js
+++ dignity/dignity_foundation/doctype/dignity_senior_citizen/dignity_senior_citizen_list.js
@@ -12,17 +12,17 @@ frappe.listview_settings['Dignity Senior Citizen'] = {
 				frappe.msgprint(__("Please enable pop-ups")); return;
 			}
 		});
-		listview.page.add_menu_item(__("Print Bulk (Marathi)"), function() {
-			names=[];
-			$.each(listview.get_checked_items(), function(key, value){
-				names.push(value._name);
-			});
-			var w = window.open("/api/method/dignity.api.bulk_print_memberships?"
-							+"names="+encodeURIComponent(names)+"&lang=MAR");
+		// listview.page.add_menu_item(__("Print Bulk (Marathi)"), function() {
+		// 	names=[];
+		// 	$.each(listview.get_checked_items(), function(key, value){
+		// 		names.push(value._name);
+		// 	});
+		// 	var w = window.open("/api/method/dignity.api.bulk_print_memberships?"
+		// 					+"names="+encodeURIComponent(names)+"&lang=MAR");
 	
-			if(!w) {
-				frappe.msgprint(__("Please enable pop-ups")); return;
-			}
-		});
+		// 	if(!w) {
+		// 		frappe.msgprint(__("Please enable pop-ups")); return;
+		// 	}
+		// });
 	}
 };
